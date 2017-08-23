750e6a73ec0f6b7166a9cc1e1c54bf4e2e1feeb1
Mirsla <844874989@qq.com>
2017-08-23 23:12:36 +0800

    Initial commit

---
+++ src/main/java/com/web/mirsla/SpringBootWorkApplication.java
@@ -0,0 +1,12 @@
+package com.web.mirsla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWorkApplication.class, args);
+	}
+}
