560fd6e0bb47601f069097bc1ece0240075eaad4
Mirsla <844874989@qq.com>
2017-11-05 21:56:04 +0800

    BaseController新增，解决thymeleaf在BaseController被继承的情况下不能使用的问题

--- src/main/java/com/web/mirsla/controller/BaseController.java
+++ src/main/java/com/web/mirsla/controller/BaseController.java
@@ -1,16 +1,30 @@
 package com.web.mirsla.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Author: Mirsla
- * @DescripTion:
+ * @DescripTion: spirngBoot中的BaseController需要添加Controller注解，不然在使用过程中会有意想不到的问题
  * @Date: Created in  2017.10.11
  * @Modified By:登录相关
  */
 
+@Controller
 public class BaseController {
 
     public String getAuthUrl(){
         return null;
     }
 
+    /**
+     * 获取request请求
+     * @return
+     */
+    protected HttpServletRequest getRequest(){
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
 }
